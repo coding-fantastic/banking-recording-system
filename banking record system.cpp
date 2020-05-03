51155ecfe49f2d66efdf203ff3fccc3f2d975c90
coding-fantastic <alexnzioka@protonmail.com>
2020-05-03 23:05:41 +0300

    first commit

---
+++ banking record system.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::fstream;
+using std::ofstream;
+using std::ifstream;
+using std::ios;
+class account_query
+{
+private:
+    char account_number[20];
+    char firstName[10];
+    char lastName[10];
+    float total_Balance;
+public:
+    void read_data();
+    void show_data();
+    void write_rec();
+    void read_rec();
+    void search_rec();
+    void edit_rec();
+    void delete_rec();
+};
+void account_query::read_data()
+{
+    cout<<"\nEnter Account Number: ";
+    cin>>account_number;
+    cout<<"Enter First Name: ";
+    cin>>firstName;
+    cout<<"Enter Last Name: ";
+    cin>>lastName;
+    cout<<"Enter Balance: ";
+    cin>>total_Balance;
+    cout<<endl;
+}
+
+int main()
+{
+    account_query A;
+    int choice;
+    cout<<"***Acount Information System***"<<endl;
+    while(true)
+    {
+        cout<<"Select one option below ";
+        cout<<"\n\t1-->Add record to file";
+        cout<<"\n\t2-->Show record from file";
+        cout<<"\n\t3-->Search Record from file";
+        cout<<"\n\t4-->Update Record";
+        cout<<"\n\t5-->Delete Record";
+        cout<<"\n\t6-->Quit";
+        cout<<"\nEnter your choice: ";
+        cin>>choice;
+        
+    }
+    system("pause");
+    return 0;
+}
